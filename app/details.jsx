a63231a2d0bdb2fd5e5a97f18ed9f96635e74293
Bhavuk Mittal <vvbnmittal@gmail.com>
2025-01-02 21:48:06 +0530

    added edit button in header

--- app/details.jsx
+++ app/details.jsx
@@ -1,25 +1,63 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack } from "expo-router";
 
 export default function DetailView() {
   const router = useRouter();
   const { contact } = useLocalSearchParams();
   const contactDetails = JSON.parse(contact);
 
+  const handleEditContact = () => {
+    router.push({
+      pathname: "add-edit",
+      params: { contact: JSON.stringify(contactDetails) },
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Name:</Text>
-      <Text style={styles.value}>{contactDetails.name}</Text>
+    <>
+      <Stack.Screen
+        options={{
+          headerTitle: "Contact Details",
+          headerRight: () => {
+            return (
+              <TouchableOpacity onPress={handleEditContact}>
+                <Ionicons name="pencil" size={24} color="black" />
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
 
-      <Text style={styles.label}>Phone:</Text>
-      <Text style={styles.value}>{contactDetails.phone}</Text>
+      <View style={styles.container}>
+        <View
+          style={{
+            width: 100,
+            height: 100,
+            borderColor: "lightgray",
+            borderWidth: 1,
+            alignSelf: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 50,
+            margin: 18,
+          }}
+        >
+          {/* <Ionicons name="camera-outline" size={32} color={"lightgray"} /> */}
+          <Text style={{ fontSize: 32, color: "lightgray" }}>
+            {contactDetails.name[0]}
+          </Text>
+        </View>
+        <Text style={styles.title}>{contactDetails.name}</Text>
+        <Text style={styles.detailText}>Phone: {contactDetails.phone}</Text>
+        <Text style={styles.detailText}>Email: {contactDetails.email}</Text>
+        {/* <Text style={styles.detailText}>Address: {contactDetails.address}</Text> */}
 
-      <Text style={styles.label}>Email:</Text>
-      <Text style={styles.value}>{contactDetails.email}</Text>
-
-      <Text style={styles.label}>Address:</Text>
-      <Text style={styles.value}>{contactDetails.address}</Text>
-    </View>
+        <Text style={styles.label}>Address:</Text>
+        <Text style={styles.value}>{contactDetails.address}</Text>
+      </View>
+    </>
   );
 }
 
@@ -27,6 +65,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  detailText: {
+    fontSize: 16,
+    textAlign: "center",
   },
   label: {
     fontSize: 14,
@@ -36,17 +83,5 @@ const styles = StyleSheet.create({
   value: {
     fontSize: 16,
     marginBottom: 12,
-  },
-  backButton: {
-    backgroundColor: "#007BFF",
-    padding: 12,
-    borderRadius: 8,
-    alignItems: "center",
-    marginTop: 20,
-  },
-  backButtonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
   },
 });
