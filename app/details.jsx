8888a60c7f8ab2273f7ced16a08cfee205d82f6b
Bhavuk Mittal <vvbnmittal@gmail.com>
2025-01-03 12:47:32 +0530

    handled delete contact

--- app/details.jsx
+++ app/details.jsx
@@ -1,12 +1,35 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Button,
+} from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { Stack } from "expo-router";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { deleteContact } from "../redux/contactSlice";
 
 export default function DetailView() {
   const router = useRouter();
+  const dispatch = useDispatch();
   const { contact } = useLocalSearchParams();
   const contactDetails = JSON.parse(contact);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleDeleteContact = () => {
+    dispatch(deleteContact(contactDetails.id));
+    setShowModal(false);
+    router.back();
+  };
+
+  const handleCancelDelete = () => {
+    setShowModal(false);
+  };
 
   const handleEditContact = () => {
     router.push({
@@ -22,32 +45,24 @@ export default function DetailView() {
           headerTitle: "Contact Details",
           headerRight: () => {
             return (
-              <TouchableOpacity onPress={handleEditContact}>
-                <Ionicons name="pencil" size={24} color="black" />
-              </TouchableOpacity>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ paddingRight: 8 }}>
+                  <TouchableOpacity onPress={handleEditContact}>
+                    <Ionicons name="pencil" size={24} color="black" />
+                  </TouchableOpacity>
+                </View>
+                <TouchableOpacity onPress={() => setShowModal(true)}>
+                  <Ionicons name="trash-outline" size={24} color="red" />
+                </TouchableOpacity>
+              </View>
             );
           },
         }}
       />
 
       <View style={styles.container}>
-        <View
-          style={{
-            width: 100,
-            height: 100,
-            borderColor: "lightgray",
-            borderWidth: 1,
-            alignSelf: "center",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: 50,
-            margin: 18,
-          }}
-        >
-          {/* <Ionicons name="camera-outline" size={32} color={"lightgray"} /> */}
-          <Text style={{ fontSize: 32, color: "lightgray" }}>
-            {contactDetails.name[0]}
-          </Text>
+        <View style={styles.avatar}>
+          <Text style={styles.avatarText}>{contactDetails.name[0]}</Text>
         </View>
         <Text style={styles.title}>{contactDetails.name}</Text>
         <Text style={styles.detailText}>Phone: {contactDetails.phone}</Text>
@@ -57,6 +72,30 @@ export default function DetailView() {
         <Text style={styles.label}>Address:</Text>
         <Text style={styles.value}>{contactDetails.address}</Text>
       </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={handleCancelDelete}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Delete Contact</Text>
+            <Text style={styles.modalMessage}>
+              Are you sure you want to delete this contact?
+            </Text>
+            <View style={styles.modalActions}>
+              <Button title="No" onPress={handleCancelDelete} color="gray" />
+              <Button
+                title="Proceed"
+                onPress={handleDeleteContact}
+                color="red"
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </>
   );
 }
@@ -65,6 +104,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderColor: "lightgray",
+    borderWidth: 1,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    margin: 18,
+  },
+  avatarText: {
+    fontSize: 32,
+    color: "lightgray",
   },
   title: {
     fontSize: 24,
@@ -83,5 +137,33 @@ const styles = StyleSheet.create({
   value: {
     fontSize: 16,
     marginBottom: 12,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  modalMessage: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  modalActions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
   },
 });
